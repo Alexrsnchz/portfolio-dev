33f8c13921a75e6458b32456e688316bd4548edb
Alexrsnchz <alexrsnchz@gmail.com>
2025-07-22 14:04:03 +0200

    refactor: Changed the naming of the translation arg to match convention

--- src/components/navigation/footer/FooterLinks.tsx
+++ src/components/navigation/footer/FooterLinks.tsx
@@ -8,7 +8,7 @@ export default function FooterLinks() {
     <div className="flex items-center gap-8">
       <a
         href="https://github.com/Alexrsnchz"
-        aria-label={t('GitHubAria')}
+        aria-label={t('gitHubAria')}
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -17,14 +17,14 @@ export default function FooterLinks() {
 
       <a
         href="https://linkedin.com/in/alejandrorsnchz/"
-        aria-label={t('LinkedInAria')}
+        aria-label={t('linkedInAria')}
         target="_blank"
         rel="noopener noreferrer"
       >
         <LinkedInIcon className="size-5 text-gray-700 dark:text-gray-400 hover:text-sky-300 dark:hover:text-purple-400 transition-color duration-200 ease-in-out" />
       </a>
 
-      <a href="mailto:alexrsnchz7@gmail.com" aria-label={t('EmailAria')}>
+      <a href="mailto:alexrsnchz7@gmail.com" aria-label={t('emailAria')}>
         <EnvelopeIcon className="size-5 text-gray-700 dark:text-gray-400 hover:text-sky-300 dark:hover:text-purple-400 transition-color duration-200 ease-in-out" />
       </a>
     </div>
